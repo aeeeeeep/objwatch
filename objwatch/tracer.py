a23254b5502b1af2d559c47c1b82c1d76f8469c2
aeeeeeep <aeeeeeep@proton.me>
2024-12-16 23:47:54 +0800

    fea: move event handls logic out of tracer.py (#8)

--- objwatch/tracer.py
+++ objwatch/tracer.py
@@ -3,6 +3,7 @@ import logging
 import importlib
 from .utils.weak import WeakTensorKeyDictionary
 from .wrappers import FunctionWrapper
+from .event_handls import EventHandls
 
 try:
     import torch
@@ -59,6 +60,7 @@ class Tracer:
         func_info = {}
         func_name = frame.f_code.co_name
         func_info['func_name'] = func_name
+        func_info['frame'] = frame
 
         if 'self' in frame.f_locals:
             obj = frame.f_locals['self']
@@ -76,15 +78,6 @@ class Tracer:
             func_info['is_method'] = False
 
         return func_info
-
-    def _determine_change_type(self, old_value, current_value):
-        if isinstance(old_value, (list, set, dict)) and isinstance(current_value, type(old_value)):
-            diff = len(current_value) - len(old_value)
-            if diff > 0:
-                return "apd"
-            elif diff < 0:
-                return "pop"
-        return "upd"
 
     def trace_func_factory(self):
         def trace_func(frame, event, arg):
@@ -108,44 +101,14 @@ class Tracer:
 
             if event == "call":
                 func_info = self._get_function_info(frame, event)
-                func_name = func_info['func_name']
-
-                if func_info.get('is_method', False):
-                    class_name = func_info['class_name']
-                    logger_msg = f"run {class_name}.{func_name}"
-                else:
-                    logger_msg = f"run {func_name}"
-
-                if self.function_wrapper:
-                    call_msg = self.function_wrapper.wrap_call(func_name, frame)
-                    logger_msg += call_msg
-
-                prefix = "| " * self.call_depth
-                logger.debug(f"{rank_info}{prefix}{logger_msg}")
-
+                EventHandls.handle_run(func_info, self.function_wrapper, self.call_depth, rank_info)
                 self.call_depth += 1
-
                 return trace_func
 
             elif event == "return":
                 self.call_depth -= 1
-
                 func_info = self._get_function_info(frame, event)
-                func_name = func_info['func_name']
-
-                if func_info.get('is_method', False):
-                    class_name = func_info['class_name']
-                    logger_msg = f"end {class_name}.{func_name}"
-                else:
-                    logger_msg = f"end {func_name}"
-
-                if self.function_wrapper:
-                    return_msg = self.function_wrapper.wrap_return(func_name, arg)
-                    logger_msg += return_msg
-
-                prefix = "| " * self.call_depth
-                logger.debug(f"{rank_info}{prefix}{logger_msg}")
-
+                EventHandls.handle_end(func_info, self.function_wrapper, self.call_depth, rank_info, arg)
                 return trace_func
 
             elif event == "line":
@@ -168,14 +131,17 @@ class Tracer:
                             else:
                                 eq = old_value is current_value
                             if not eq:
-                                # if old_value != current_value:
-                                change_type = self._determine_change_type(old_value, current_value)
+                                change_type = EventHandls.determine_change_type(old_value, current_value)
                                 if change_type != "upd":
                                     diff_msg = f" {len(old_value)} -> {len(current_value)}"
                                 else:
                                     diff_msg = ""
-                                prefix = "| " * self.call_depth
-                                logger.debug(f"{rank_info}{prefix}{change_type} {class_name}.{key}{diff_msg}")
+                                if change_type == "upd":
+                                    EventHandls.handle_upd(class_name, key, diff_msg, self.call_depth, rank_info)
+                                elif change_type == "apd":
+                                    EventHandls.handle_apd(class_name, key, diff_msg, self.call_depth, rank_info)
+                                elif change_type == "pop":
+                                    EventHandls.handle_pop(class_name, key, diff_msg, self.call_depth, rank_info)
                                 old_attrs[key] = current_value
                 return trace_func
 
