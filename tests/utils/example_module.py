75638e9dcf7ba818743ba0ba6ee3249e8b3e67ee
aeeeeeep <aeeeeeep@proton.me>
2024-12-21 01:10:58 +0800

    feat: more ut cases

---
+++ tests/utils/example_module.py
@@ -0,0 +1,19 @@
+def test_func():
+    return "result"
+
+
+def custom_func(arg1):
+    return f"custom_result with {arg1}"
+
+
+class SampleClass:
+    def __init__(self, value):
+        self.value = value
+
+    def increment(self):
+        self.value += 1
+        return self.value
+
+    def decrement(self):
+        self.value -= 1
+        return self.value
