a07275dbc8a5e7b7bfc119d83a17933b7588ff6a
webbrain-one <295484252+webbrain-one@users.noreply.github.com>
2026-08-02 20:46:41 +0300

    Add support for additional trace events
    Extend objwatch to handle exception, c_call, c_return, and c_exception
    trace events. Previously, only line, call, and return were supported.
    
    Fixes #81

--- objwatch/events.py
+++ objwatch/events.py
@@ -24,6 +24,18 @@ class EventType(Enum):
     # Marks the removal of elements from data structures like lists, tuple, sets, or dictionaries.
     POP = 5
 
+    # Indicates an exception has occurred.
+    EXCEPTION = 6
+
+    # Indicates a C function is about to be called.
+    C_CALL = 7
+
+    # Indicates a C function has returned.
+    C_RETURN = 8
+
+    # Indicates a C function has raised an exception.
+    C_EXCEPTION = 9
+
     def __init__(self, value):
-        labels = {1: 'run', 2: 'end', 3: 'upd', 4: 'apd', 5: 'pop'}
+        labels = {1: 'run', 2: 'end', 3: 'upd', 4: 'apd', 5: 'pop', 6: 'exception', 7: 'c_call', 8: 'c_return', 9: 'c_exception'}
         self.label = labels[value]
